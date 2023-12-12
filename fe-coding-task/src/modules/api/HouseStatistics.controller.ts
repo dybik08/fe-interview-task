7eca4f17055eb68f2e511468fead8db5f895d844
Wojciech Dybikowski <dybik08@gmail.com>
2023-12-13 00:34:44 +0100

    feature - changes to houseStatisticsListQueryKey - fix chart not reloading

--- fe-coding-task/src/modules/api/HouseStatistics.controller.ts
+++ fe-coding-task/src/modules/api/HouseStatistics.controller.ts
@@ -1,11 +1,11 @@
 import {useQuery} from "react-query";
 import { DateRangeApiFormat, HouseStatisticsApi, HouseType } from "./HouseStatistics.api";
 
-export const houseStatisticsListQueryKey = () => ["houseStatisticsApi.getHouseStatistics"]
+export const houseStatisticsListQueryKey = (numberOfQuarters: number) => ["houseStatisticsApi.getHouseStatistics", numberOfQuarters]
 
 export const useListHouseStatistics = (houseType: HouseType[], dateRange: DateRangeApiFormat) => {
     const houseStatisticsApi = new HouseStatisticsApi()
-    const { data: houseStatistics = [], isLoading: loading, error } = useQuery<any, Error>(houseStatisticsListQueryKey(), async () => {
+    const { data: houseStatistics = [], isLoading: loading, error } = useQuery<any, Error>(houseStatisticsListQueryKey(dateRange.length), async () => {
             try {
                 return await houseStatisticsApi.getHouseStatistics(houseType, dateRange).then(list => list)
             } catch (error) {
