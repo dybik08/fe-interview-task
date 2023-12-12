0331ff2be536d5518158eea74d529e610ffb5167
Wojciech Dybikowski <dybik08@gmail.com>
2023-12-13 00:35:27 +0100

    feature - update ChartJS setup

--- fe-coding-task/src/App.tsx
+++ fe-coding-task/src/App.tsx
@@ -1,69 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {useListHouseStatistics} from './modules/api/HouseStatistics.controller'
-import {DateRange, DateRangeApiFormat, HouseType} from "./modules/api/HouseStatistics.api";
+import {BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip,} from 'chart.js';
+import {BarChartWrapper, DateRangeForm} from "./modules/HouseStatistics";
 
-class DateRangeMapper {
-  static mapDateRangeToQuarterList(dateRange: DateRange): DateRangeApiFormat {
-    // starting year, starting quater, fill with quaters to end year, end quater
-    const startingYear = dateRange.from.year
-    const endingYear = dateRange.to.year
-    const startingQuarter = dateRange.from.quarter
-    const endingQuarter = dateRange.to.quarter
-    
-    const result = []
-    
-    const pickEndingQuarter = (year: number) => {
-      if(year === endingYear) {
-        return endingQuarter
-      }
-      
-      return 4
-    }
-    
-    for (let year = startingYear; year <= endingYear; year++) {
-      for (let quarter = startingQuarter; quarter <= pickEndingQuarter(year); quarter++) {
-        const correctFormat = `${year}K${quarter}`;
-        result.push(correctFormat)
-      }
-    }
-    
-    return result
-  }
-}
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 function App() {
-  const dateRange = DateRangeMapper.mapDateRangeToQuarterList({
-    from: {
-      year: 2020,
-      quarter: 1
-    },
-    to: {
-      year: 2022,
-      quarter: 2
-    },
-  })
-  console.log({ dateRange });
-  const {error, loading, houseStatistics} = useListHouseStatistics([HouseType.Smahus], dateRange )
-  console.log("houseStatistics: ", houseStatistics)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="flex m-10 justify-center items-center" >
+          <div className="p-10 flex flex-col justify-center items-center bg-white rounded-3xl">
+              <header className="px-10">
+                  <p className="py-10">
+                      Norway statistics on the average price per square meter
+                  </p>
+              </header>
+              <DateRangeForm/>
+              <BarChartWrapper/>
+          </div>
+      </div>
   );
 }
 
