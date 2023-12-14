7d1f5bb7e980ac4cd6a0a0e19680f41ed1a9a466
Wojciech Dybikowski <dybik08@gmail.com>
2023-12-14 23:48:49 +0100

    feature - update AppProvider - add ApiContextProvider

--- fe-coding-task/src/modules/app/AppProvider.tsx
+++ fe-coding-task/src/modules/app/AppProvider.tsx
@@ -1,16 +1,19 @@
 import {QueryClient} from "react-query";
 import {PropsWithChildren} from "react";
 import {ReactQueryProvider} from "./react-query";
+import {ApiContextProvider} from "../api/Api.context";
 
 
 interface IAppProviderProps {
     queryClient?: QueryClient
 }
 
-export const AppProvider = ({ children, queryClient }: PropsWithChildren<IAppProviderProps>) => {
+export const AppProvider = ({ children }: PropsWithChildren<IAppProviderProps>) => {
     return (
         <ReactQueryProvider>
-            {children}
+            <ApiContextProvider>
+                {children}
+            </ApiContextProvider>
         </ReactQueryProvider>
     )
 }
